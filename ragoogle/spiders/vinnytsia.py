0855b8461dce55b4c921ff107c5fd9b6bb9b8b6f
koliambus <koliambus@ukr.net>
2019-09-30 16:06:29 +0300

    add Vinnytsia Spider

---
+++ ragoogle/spiders/vinnytsia.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy import FormRequest
+
+from ragoogle.items.vinnytsia import MbuItem
+from ragoogle.loaders import StripJoinItemLoader
+
+
+class VinnytsiaSpider(scrapy.Spider):
+    name = "vinnytsia"
+    allowed_domains = ["vmr.gov.ua"]
+    start_urls = ["https://www.vmr.gov.ua/Executives/SitePages/ArchitectureRegistry.aspx"]
+    custom_settings = {
+        # specifies exported fields and order
+        'FEED_EXPORT_FIELDS': ["order_no", "order_date", "customer", "obj", "address", "changes", "cancellation",
+                               "scan_url"],
+    }
+
+    def parse(self, response):
+        for row in response.css('table.ms-listviewtable tr[class^=building-registry-row]'):
+            l = StripJoinItemLoader(item=MbuItem(), selector=row)
+            l.add_css("order_no", "td:nth-child(1)::text")
+            l.add_css("number_in_order", "td:nth-child(2)::text")
+            l.add_css("order_date", "td:nth-child(3)::text")
+            l.add_css("decree_no", "td:nth-child(4)::text")
+            l.add_css("customer", "td:nth-child(5) div::text")
+            l.add_css("obj", "td:nth-child(6) div::text")
+            l.add_css("address", "td:nth-child(7) div::text")
+            l.add_css("changes", "td:nth-child(8) div::text")
+            l.add_css("cancellation", "td:nth-child(9) div::text")
+
+            url = row.css("td:nth-child(10) a::attr(href)").extract_first()
+            l.add_value("scan_url", response.urljoin(url))
+            yield l.load_item()
+
+        # get next page number href next to current inactive with span tag
+        nextPageJs = response.xpath('//table[@class="ms-listviewtable"]//tr[@class="building-registry-pager"]//table/tr/td[.//span]/following-sibling::td[1]/a/@href')
+        if len(nextPageJs):
+            yield FormRequest.from_response(
+                response,
+                formname="aspnetForm",
+                formxpath="//form[@id='aspnetForm']",
+                dont_click=True,
+                formdata={
+                    '__EVENTARGUMENT': nextPageJs.re('\',\'(.*)\'\)'),
+                    '__EVENTTARGET': nextPageJs.re('javascript:__doPostBack\(\'(.*)\',')
+                },
+                dont_filter=True,
+                callback=self.parse
+            )
+
+    def parse_results(self, response):
+        for row in response.css('table.ms-listviewtable tr[class^=building-registry-row]'):
+            l = StripJoinItemLoader(item=MbuItem(), selector=row)
+            l.add_css("order_no", "td:nth-child(1)::text")
+            l.add_css("number_in_order", "td:nth-child(2)::text")
+            l.add_css("order_date", "td:nth-child(3)::text")
+            l.add_css("decree_no", "td:nth-child(4)::text")
+            l.add_css("customer", "td:nth-child(5)::text")
+            l.add_css("obj", "td:nth-child(6)::text")
+            l.add_css("address", "td:nth-child(7)::text")
+            l.add_css("changes", "td:nth-child(8)::text")
+            l.add_css("cancellation", "td:nth-child(9)::text")
+
+            url = row.css("td:nth-child(10) a::attr(href)").extract_first()
+            l.add_value("scan_url", response.urljoin(url))
+            yield l.load_item()
